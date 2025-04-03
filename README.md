505aff44276fa57457f503bbda2584722d1b0c29
CreeperCatcherD <49421694+CreeperCatcherD@users.noreply.github.com>
2025-04-03 16:27:58 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+This is the ROS2-Jazzy programming environment for the Robotic Cardinals VEXU robotics team(s) powered by the nix package manager.
+For instalation of the nix package manager, see the official nix documentation at:
+[The NixOS Website](https://nixos.org/download/)
+Once installed, clone this repository and, in the cloned directory, run
+'''bash enter-shell.sh '''
+or on NixOS systems with flakes enabled, simply run
+'''nix develop'''
+to setup and enter the development environment.
---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  inputs = {
+    nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay/ab3c1b3b7c3eca52614b4fe9d7c05ddded5b94b1";
+    nixpkgs.url = "github:lopsided98/nixpkgs?ref=nix-ros";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils, nix-ros-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ nix-ros-overlay.overlays.default ];
+          config.permittedInsecurePackages = [
+            "freeimage-unstable-2021-11-01"
+          ];
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "ros2-gz-dev";
+          packages = with pkgs; with pkgs.rosPackages.jazzy; [
+            colcon
+            ros-core
+            ament-cmake-core
+            python-cmake-module
+            gz-cmake-vendor
+            gz-common-vendor
+            gz-dartsim-vendor
+            gz-fuel-tools-vendor
+            gz-gui-vendor
+            gz-launch-vendor
+            gz-math-vendor
+            gz-msgs-vendor
+            gz-ogre-next-vendor
+            gz-physics-vendor
+            gz-plugin-vendor
+            gz-rendering-vendor
+            gz-sensors-vendor
+            gz-sim-vendor
+            gz-tools-vendor
+            gz-transport-vendor
+            gz-utils-vendor
+          ];
+          shellHook = ''
+            unset QT_QPA_PLATFORM
+          '';
+        };
+      }) // {
+        nixConfig = {
+          extra-substituters = [ "https://ros.cachix.org" ];
+          extra-trusted-public-keys = [ "ros.cachix.org-1:dSyZxI8geDCJrwgvCOHDoAfOm5sV1wCPjBkKL+38Rvo=" ];
+        };
+      };
+}
